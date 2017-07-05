06770986f1f665e238f07523e53f2f4e3dad30ea
Tom Peacock <thomas.s.peacock@gmail.com>
2017-07-05 17:20:40 +0100

    more general script for multiple cells

--- run_multiple_files.sh
+++ run_multiple_files.sh
@@ -1,4 +1,10 @@
  #!/bin/bash
+
+function removepath {
+	b=$(basename $1)
+	echo $b
+}
+
 function decombine {
  		if [ "$2" == "a" ]
  			then chain="alpha"
@@ -8,12 +14,13 @@ function decombine {
  		python SC_Decombinator.py -fq $1 -c $chain -o $3 -nbc
  		echo " "
  		echo "unzipping..."
-		length=${#fastq_file}
+		remove_path=$(removepath $fastq_file)
+		length=${#remove_path}
 		remove_ext=$(($length - 6))
-		gzip -d "./dcr_"$chain"_"${fastq_file:0:remove_ext}".n12.gz"
+		gzip -d "./dcr_"$chain"_"${remove_path:0:remove_ext}".n12.gz"
 		echo " "
 		echo "moving to results folder..."
-		mv "./dcr_"$chain"_"${fastq_file:0:remove_ext}".n12" $directory"/dcr_"$chain"_"${fastq_file:0:remove_ext}"_"$3".n12"
+		mv "./dcr_"$chain"_"${remove_path:0:remove_ext}".n12" $directory"/dcr_"$chain"_"${remove_path:0:remove_ext}"_"$3".n12"
 		echo " "
 		echo "cleaning up Decombinator..."
 }
