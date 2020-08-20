893332a37c722e80180928c4d94ab22c0735be58
Tom Peacock <thomas.s.peacock@gmail.com>
2020-08-20 17:04:49 +0100

    move scripts out to Decombinator-Tools repository

--- Decombinator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Bash version ${BASH_VERSION}..."
-#running Decombinator though loop 
-for file in LTX_*.fq
-do 
-python Decombinator.py -fq file -sp human 
-done
--- Demultiplexor.sh
+++
@@ -1,4 +0,0 @@
-#running Demultiplexor pipeline-
-python Demultiplexor.py -r1 Undetermined_S0_R1_001.fastq.gz -r2 Undetermined_S0_R2_001.fastq.gz -i1 Undetermined_S0_I1_001.fastq.gz -ix NS051_SampleIndexSheet.csv
-
-
