85cf209eb6d100369a6c14577790ad15d5fda3a8
MVCowley <51127523+MVCowley@users.noreply.github.com>
2026-03-04 12:24:36 +0000

    test(collapse): add read_in_dcr collapsing tests

--- tests/test_collapse.py
+++ tests/test_collapse.py
@@ -199,18 +199,126 @@ class TestReadInData:
     collapse.counts = coll.Counter()
 
     @pytest.fixture
-    def blank_input(self):
+    def blank_input(self) -> list[None]:
         return []
 
     @pytest.fixture
-    def pipe_args(self):
-        return {"command": "pipeline"}
+    def pipe_args(self) -> dict[str, str | int]:
+        return {
+            "command": "pipeline",
+            "lenthreshold": 130,
+            "minbcQ": 20,
+            "bcQbelowmin": 1,
+            "avgQthreshold": 30,
+            "oligo": "M13",
+            "sampling_analysis": False,
+        }
 
     def test_no_dcr(self, blank_input, pipe_args):
         with pytest.raises(ValueError):
             collapse.read_in_data(
                 blank_input, pipe_args, None, None, None, None
             )
+
+    @pytest.fixture
+    def valid_input(self) -> list[list[str]]:
+        return [
+            [
+                "15",
+                "4",
+                "1",
+                "7",
+                "CCCCCAGGGGGCTC",
+                "LH00409:259:22JJCFLT4:8:1149:38410:17375",
+                "ATCCTGAAGACAGCAGCTTCTACATCTGCAGTGCTAGAGCCCCCAGGGGGCTCCAGCCCCAGCATTTTGGTGATGGGACTCGACTC",
+                "IIIIIIIIIIIIIII-II-IIIIIIIIIIIIIIIIIIIIIIIIIIIII-IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII",
+                "GTCGTGACTGGGAAAACCCTGGCACCCGGTCGTGATCTGACT",
+                "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII",
+                "GACACAACTCTCCCCAGAGAAGGTGGTGTGA",
+            ],
+            [  # Final base of junction is changed, should still collapse
+                "15",
+                "4",
+                "1",
+                "7",
+                "CCCCCAGGGGGCTG",
+                "LH00409:259:22JJCFLT4:8:1149:38410:17375",
+                "ATCCTGAAGACAGCAGCTTCTACATCTGCAGTGCTAGAGCCCCCAGGGGGCTGCAGCCCCAGCATTTTGGTGATGGGACTCGACTC",
+                "IIIIIIIIIIIIIII-II-IIIIIIIIIIIIIIIIIIIIIIIIIIIII-IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII",
+                "GTCGTGACTGGGAAAACCCTGGCACCCGGTCGTGATCTGACT",
+                "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII",
+                "GACACAACTCTCCCCAGAGAAGGTGGTGTGA",
+            ],
+            [  # Same TCR but with different junction region, making TCR 20% different
+                "15",
+                "4",
+                "1",
+                "7",
+                "AAAAAAAAAAAAAA",
+                "LH00409:259:22JJCFLT4:8:1149:38410:17375",
+                "ATCCTGAAGACAGCAGCTTCTACATCTGCAGTGCTAGAGAAAAAAAAAAAAAACAGCCCCAGCATTTTGGTGATGGGACTCGACTC",
+                "IIIIIIIIIIIIIII-II-IIIIIIIIIIIIIIIIIIIIIIIIIIIII-IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII",
+                "GTCGTGACTGGGAAAACCCTGGCACCCGGTCGTGATCTGACT",
+                "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII",
+                "GACACAACTCTCCCCAGAGAAGGTGGTGTGA",
+            ],
+        ]
+
+    @pytest.fixture
+    def barcode_quality_parameters(
+        self, pipe_args: dict[str, int]
+    ) -> list[int]:
+        return [
+            pipe_args["minbcQ"],
+            pipe_args["bcQbelowmin"],
+            pipe_args["avgQthreshold"],
+        ]
+
+    def test_barcode_collision(
+        self,
+        valid_input: list[list[str]],
+        pipe_args: dict[str, str | int],
+        barcode_quality_parameters: list[int],
+    ):
+        barcode_dcretc = collapse.read_in_data(
+            valid_input,
+            pipe_args,
+            barcode_quality_parameters,
+            lev_threshold_fraction=0.1,
+            dont_count=False,
+            opener=open,
+        )
+        assert barcode_dcretc == {
+            "CACCCGCTGACT|0|ATCCTGAAGACAGCAGCTTCTACATCTGCAGTGCTAGAGCCCCCAGGGGGCTCCAGCCCCAGCATTTTGGTGATGGGACTCGACTC": [
+                "['15', '4', '1', '7', 'CCCCCAGGGGGCTC']|ATCCTGAAGACAGCAGCTTCTACATCTGCAGTGCTAGAGCCCCCAGGGGGCTCCAGCCCCAGCATTTTGGTGATGGGACTCGACTC|IIIIIIIIIIIIIII-II-IIIIIIIIIIIIIIIIIIIIIIIIIIIII-IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII|LH00409:259:22JJCFLT4:8:1149:38410:17375",
+                "['15', '4', '1', '7', 'CCCCCAGGGGGCTG']|ATCCTGAAGACAGCAGCTTCTACATCTGCAGTGCTAGAGCCCCCAGGGGGCTGCAGCCCCAGCATTTTGGTGATGGGACTCGACTC|IIIIIIIIIIIIIII-II-IIIIIIIIIIIIIIIIIIIIIIIIIIIII-IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII|LH00409:259:22JJCFLT4:8:1149:38410:17375",
+            ],
+            "CACCCGCTGACT|1|ATCCTGAAGACAGCAGCTTCTACATCTGCAGTGCTAGAGAAAAAAAAAAAAAACAGCCCCAGCATTTTGGTGATGGGACTCGACTC": [
+                "['15', '4', '1', '7', 'AAAAAAAAAAAAAA']|ATCCTGAAGACAGCAGCTTCTACATCTGCAGTGCTAGAGAAAAAAAAAAAAAACAGCCCCAGCATTTTGGTGATGGGACTCGACTC|IIIIIIIIIIIIIII-II-IIIIIIIIIIIIIIIIIIIIIIIIIIIII-IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII|LH00409:259:22JJCFLT4:8:1149:38410:17375"
+            ],
+        }
+
+    def test_barcode_collision_no_tcr_check(
+        self,
+        valid_input: list[list[str]],
+        pipe_args: dict[str, str | int],
+        barcode_quality_parameters: list[int],
+    ):
+        barcode_dcretc = collapse.read_in_data(
+            valid_input,
+            pipe_args,
+            barcode_quality_parameters,
+            lev_threshold_fraction=1,
+            dont_count=False,
+            opener=open,
+        )
+        assert barcode_dcretc == {
+            "CACCCGCTGACT|0|ATCCTGAAGACAGCAGCTTCTACATCTGCAGTGCTAGAGCCCCCAGGGGGCTCCAGCCCCAGCATTTTGGTGATGGGACTCGACTC": [
+                "['15', '4', '1', '7', 'CCCCCAGGGGGCTC']|ATCCTGAAGACAGCAGCTTCTACATCTGCAGTGCTAGAGCCCCCAGGGGGCTCCAGCCCCAGCATTTTGGTGATGGGACTCGACTC|IIIIIIIIIIIIIII-II-IIIIIIIIIIIIIIIIIIIIIIIIIIIII-IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII|LH00409:259:22JJCFLT4:8:1149:38410:17375",
+                "['15', '4', '1', '7', 'CCCCCAGGGGGCTG']|ATCCTGAAGACAGCAGCTTCTACATCTGCAGTGCTAGAGCCCCCAGGGGGCTGCAGCCCCAGCATTTTGGTGATGGGACTCGACTC|IIIIIIIIIIIIIII-II-IIIIIIIIIIIIIIIIIIIIIIIIIIIII-IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII|LH00409:259:22JJCFLT4:8:1149:38410:17375",
+                "['15', '4', '1', '7', 'AAAAAAAAAAAAAA']|ATCCTGAAGACAGCAGCTTCTACATCTGCAGTGCTAGAGAAAAAAAAAAAAAACAGCCCCAGCATTTTGGTGATGGGACTCGACTC|IIIIIIIIIIIIIII-II-IIIIIIIIIIIIIIIIIIIIIIIIIIIII-IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII|LH00409:259:22JJCFLT4:8:1149:38410:17375",
+            ],
+        }
 
 
 class TestCheckDcrFile:
